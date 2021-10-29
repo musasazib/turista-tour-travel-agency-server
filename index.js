9a43c5982e0a538203d263e170611cf3e706a574
musasazib <musasazib@gmail.com>
2021-10-29 15:51:01 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyv3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('turistaTravel');
+        const servicesCollection = database.collection(services);
+
+        
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
