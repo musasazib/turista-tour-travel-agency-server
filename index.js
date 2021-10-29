cfb2a46982430db2c5f41f13cea6e676a52c9e8b
musasazib <musasazib@gmail.com>
2021-10-29 16:58:53 +0600

    Added Get API

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
+const cors = require("cors");
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyv3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -13,9 +18,23 @@ async function run() {
     try {
         await client.connect();
         const database = client.db('turistaTravel');
-        const servicesCollection = database.collection(services);
+        const servicesCollection = database.collection('services');
 
-        
+        // Get API
+        app.get('/services', async (req, res) => {
+
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // Post API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('Hit the post API', service);
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        })
 
     }
     finally {
